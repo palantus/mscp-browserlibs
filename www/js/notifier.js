36638b802ee47dbc7897af66b94789f38d1e7506
Anders H. Kristiansen <palantus@gmail.com>
2017-02-11 17:11:06 +0100

    Misc changes

--- www/js/notifier.js
+++ www/js/notifier.js
@@ -1,15 +1,17 @@
 /*
 	Usage:
 	new Notifier().init({text: "You clicked on " + r.test}).show();
-	
+  notify()
+
 	Alternatives:
 	new Notifier().show("You clicked on " + r.test);
 	new Notifier().show("You clicked on " + r.test, 5000);
-	
+  notify("My message", 2000)
+
 	Options:
 	text: The text to show
 	timeout: The time that the notification should be shown. Default: 2000 ms.
-	
+
 	Notes:
 		- Position: It will automatically move above a taskbar, if one exists
 */
@@ -27,25 +29,27 @@ Notifier.prototype.init = function(_options){
 
 Notifier.prototype.show = function(text, timeout){
 	var t = this;
-	
+
 	if(t.options.text === undefined)
 		t.options.text = text;
-	
+
 	if(t.options.timeout === undefined)
 		t.options.timeout = timeout;
-	
+
 	this.element = $("<div/>", {class: "notifier"});
 	this.element.append(this.options.text);
-	
+
 	if($("div.tbcbar").length > 0)
 		this.element.css("transform", "translate(0px,-30px)");
-	
+
 	$("body").append(this.element);
 	this.element.fadeIn("fast");
-	
+
 	setTimeout(function(){
 		t.element.fadeOut("fast", function(){
 			t.element.remove();
 		});
 	}, isNaN(this.options.timeout) ? 2000 : this.options.timeout);
 }
+
+var notify = (message, timeout) => new Notifier().show(message, timeout);
